91d42df3116195e40d95c4b76203bb3f35d17e32
EC2 Default User <ec2-user@ip-172-31-16-71.ec2.internal>
2024-10-29 15:01:23 +0000

    comment auth ui commit

--- apps/react-amplified/src/App.tsx
+++ apps/react-amplified/src/App.tsx
@@ -37,3 +37,26 @@ export default App
 
 
 
+// auth ui https://docs.amplify.aws/react/build-a-backend/auth/set-up-auth/#connect-your-application-code-to-your-auth-resource
+
+// import type { AppProps } from 'next/app';
+// import { Authenticator } from '@aws-amplify/ui-react';
+// import { Amplify } from 'aws-amplify';
+// import outputs from '@/amplify_outputs.json';
+// import '@aws-amplify/ui-react/styles.css';
+
+// Amplify.configure(outputs);
+
+// export default function App({ Component, pageProps }: AppProps) {
+//   return (
+//     <Authenticator>
+//       {({ signOut, user }) => (
+//         <main>
+//           <h1>Hello {user?.username}</h1>
+//           <button onClick={signOut}>Sign out</button>
+//           <Component {...pageProps} />
+//         </main>
+//       )}
+//     </Authenticator>
+//   );
+// };
