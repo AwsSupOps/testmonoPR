54e9c0b9f3229f51feba58183fb4cfd6e81dd72b
EC2 Default User <ec2-user@ip-172-31-4-66.ec2.internal>
2024-11-01 18:35:38 +0000

    adding  frontend ui commit

--- apps/amplify-vite-react-template/src/App.tsx
+++ apps/amplify-vite-react-template/src/App.tsx
@@ -1,43 +1,69 @@
-import { useEffect, useState } from "react";
-//import type { Schema } from "../amplify/data/resource";
-//import { generateClient } from "aws-amplify/data";
+// import { useEffect, useState } from "react";
+// //import type { Schema } from "../amplify/data/resource";
+// //import { generateClient } from "aws-amplify/data";
 
-import { generateClient } from "aws-amplify/data";
-import type { Schema } from "@/data-schema";
+// import { generateClient } from "aws-amplify/data";
+// import type { Schema } from "@/data-schema";
 
-const client = generateClient<Schema>();
+// const client = generateClient<Schema>();
 
-function App() {
-  const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
+// function App() {
+//   const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
 
-  useEffect(() => {
-    client.models.Todo.observeQuery().subscribe({
-      next: (data) => setTodos([...data.items]),
-    });
-  }, []);
+//   useEffect(() => {
+//     client.models.Todo.observeQuery().subscribe({
+//       next: (data) => setTodos([...data.items]),
+//     });
+//   }, []);
 
-  function createTodo() {
-    client.models.Todo.create({ content: window.prompt("Todo content") });
-  }
+//   function createTodo() {
+//     client.models.Todo.create({ content: window.prompt("Todo content") });
+//   }
 
+//   return (
+//     <main>
+//       <h1>My todos</h1>
+//       <button onClick={createTodo}>+ new</button>
+//       <ul>
+//         {todos.map((todo) => (
+//           <li key={todo.id}>{todo.content}</li>
+//         ))}
+//       </ul>
+//       <div>
+//         🥳 App successfully hosted. Try creating a new todo.
+//         <br />
+//         <a href="https://docs.amplify.aws/react/start/quickstart/#make-frontend-updates">
+//           Review next step of this tutorial.
+//         </a>
+//       </div>
+//     </main>
+//   );
+// }
+
+// export default App;
+
+
+
+// adding auth ui
+
+//
+
+mport { Authenticator } from '@aws-amplify/ui-react';
+import { Amplify } from 'aws-amplify';
+import outputs from '../amplify_outputs.json';
+import '@aws-amplify/ui-react/styles.css';
+
+Amplify.configure(outputs);
+
+export default function App() {
   return (
-    <main>
-      <h1>My todos</h1>
-      <button onClick={createTodo}>+ new</button>
-      <ul>
-        {todos.map((todo) => (
-          <li key={todo.id}>{todo.content}</li>
-        ))}
-      </ul>
-      <div>
-        🥳 App successfully hosted. Try creating a new todo.
-        <br />
-        <a href="https://docs.amplify.aws/react/start/quickstart/#make-frontend-updates">
-          Review next step of this tutorial.
-        </a>
-      </div>
-    </main>
+    <Authenticator>
+      {({ signOut, user }) => (
+        <main>
+          <h1>Hello {user?.username}</h1>
+          <button onClick={signOut}>Sign out</button>
+        </main>
+      )}
+    </Authenticator>
   );
-}
-
-export default App;
+};
